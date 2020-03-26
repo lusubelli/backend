b94d5d2aae33fa72e48c4b19bcb7a0b286df3d0d
Lionel Usubelli <lionel.usubelli@ext.bpifrance.fr>
2020-03-26 08:54:26 +0100

    [TECH] totototototo

--- src/main/java/fr/usubelli/accounting/common/VertxServer.java
+++ src/main/java/fr/usubelli/accounting/common/VertxServer.java
@@ -12,31 +12,49 @@ import io.vertx.ext.auth.htpasswd.HtpasswdAuthOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BasicAuthHandler;
 
 public class VertxServer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VertxServer.class);
 
     private final Vertx vertx;
-    private final VertxRouterBuilder routerBuilder;
     private final MicroServiceConfiguration microServiceConfiguration;
     private final HttpServerOptions httpServerOptions;
+    private final Router router;
     private JWTAuth jwtAuth;
 
-    VertxServer(Vertx vertx, VertxRouterBuilder routerBuilder, MicroServiceConfiguration microServiceConfiguration) {
+    VertxServer(Vertx vertx, MicroServiceConfiguration microServiceConfiguration) {
         this.vertx = vertx;
-        this.routerBuilder = routerBuilder;
         this.microServiceConfiguration = microServiceConfiguration;
         this.httpServerOptions = new HttpServerOptions();
+        this.router = Router.router(vertx);
+        this.router.route().handler(ctx -> {
+            ctx.response()
+                    // do not allow proxies to cache the data
+                    .putHeader("Cache-Control", "no-store, no-cache")
+                    // prevents Internet Explorer from MIME - sniffing a
+                    // response away from the declared content-type
+                    .putHeader("X-Content-Type-Options", "nosniff")
+                    // Strict HTTPS (for about ~6Months)
+                    .putHeader("Strict-Transport-Security", "max-age=" + 15768000)
+                    // IE8+ do not allow opening of attachments in the context of this resource
+                    .putHeader("X-Download-Options", "noopen")
+                    // enable XSS for IE
+                    .putHeader("X-XSS-Protection", "1; mode=block")
+                    // deny frames
+                    .putHeader("X-FRAME-OPTIONS", "DENY");
+            ctx.next();
+        });
     }
 
     VertxServer htpasswd() {
         LOGGER.info(String.format("HTPASSWD : %s", microServiceConfiguration.hasBasicAuth()));
         if (this.microServiceConfiguration.hasBasicAuth()) {
-            this.routerBuilder.htpasswd(
+            router.route().handler(BasicAuthHandler.create(
                     HtpasswdAuth.create(vertx, new HtpasswdAuthOptions().setHtpasswdFile(
                             this.microServiceConfiguration.basic().getPath())),
-                    this.microServiceConfiguration.basic().getRealm());
+                    this.microServiceConfiguration.basic().getRealm()));
         }
         return this;
     }
@@ -66,16 +84,14 @@ public class VertxServer {
 
     public static VertxServer create(MicroServiceConfiguration microServiceConfiguration) {
         final Vertx vertx = Vertx.vertx(new VertxOptions());
-        final Router router = Router.router(vertx);
-        return new VertxServer(vertx, VertxRouterBuilder.router(router).protect(), microServiceConfiguration)
+        return new VertxServer(vertx, microServiceConfiguration)
                 .htpasswd()
                 .ssl()
                 .jwt();
     }
 
     public void start(VertxMicroService microService) {
-        LOGGER.info(String.format("Starting service on port : %s", microServiceConfiguration.getPort()));
-        final Router router = this.routerBuilder.build();
+        LOGGER.info(String.format("PORT : %s", microServiceConfiguration.getPort()));
         microService.route(router, jwtAuth);
         this.vertx.createHttpServer(this.httpServerOptions)
                 .requestHandler(router)
